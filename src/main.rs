c0c78fa2f5243e37d884b6edc06e21444b206ccf
Jun Kurihara <junkurihara@users.noreply.github.com>
2024-06-27 21:54:29 +0900

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,113 @@
+use anyhow::bail;
+use hyper::{service::service_fn, Request, Response};
+use std::{net::SocketAddr, sync::Arc};
+use tokio::net::TcpSocket;
+
+#[cfg(not(target_arch = "wasm32"))]
+use hyper::body::Incoming;
+#[cfg(not(target_arch = "wasm32"))]
+use hyper_util::{
+  rt::{TokioExecutor, TokioIo},
+  server::conn::auto::Builder as ConnectionBuilder,
+};
+
+#[cfg(target_arch = "wasm32")]
+use hyper::{server::conn::Http, Body};
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() {
+  println!("Hello from main.rs");
+
+  let server = HttpServer {
+    #[cfg(not(target_arch = "wasm32"))]
+    connection_builder: Arc::new(ConnectionBuilder::new(TokioExecutor::default())),
+    #[cfg(target_arch = "wasm32")]
+    connection_builder: Arc::new(Http::new()),
+  };
+  if let Err(e) = server.start().await {
+    eprintln!("Error: {:?}", e);
+  };
+}
+
+#[cfg(not(target_arch = "wasm32"))]
+async fn handle_request(_req: Request<Incoming>) -> anyhow::Result<Response<String>> {
+  println!("Handling request: {:?}", _req);
+  handler_inner(_req)
+}
+
+#[cfg(target_arch = "wasm32")]
+async fn handle_request(_req: Request<Body>) -> anyhow::Result<Response<String>> {
+  println!("Handling request: {:?}", _req);
+  handler_inner(_req)
+}
+
+fn handler_inner<T>(_req: Request<T>) -> anyhow::Result<Response<String>> {
+  match *_req.method() {
+    hyper::Method::GET => {
+      let body = "Hello from GET request";
+      Ok(Response::new(body.to_string()))
+    }
+    hyper::Method::POST => {
+      let body = "Hello from POST request";
+      Ok(Response::new(body.to_string()))
+    }
+    _ => {
+      let body = "Hello from unknown request";
+      Ok(Response::new(body.to_string()))
+    }
+  }
+}
+
+struct HttpServer {
+  #[cfg(not(target_arch = "wasm32"))]
+  connection_builder: Arc<ConnectionBuilder<TokioExecutor>>,
+  #[cfg(target_arch = "wasm32")]
+  connection_builder: Arc<Http>,
+}
+
+impl HttpServer {
+  async fn start(&self) -> anyhow::Result<()> {
+    let listening_on = SocketAddr::from(([0, 0, 0, 0], 8080));
+    let listener_service = async {
+      let tcp_socket = bind_tcp_socket(&listening_on)?;
+      let tcp_listener = tcp_socket.listen(1024)?;
+      println!("Listening on: {}", listening_on);
+
+      while let Ok((stream, client_addr)) = tcp_listener.accept().await {
+        println!("Accepted connection from: {}", client_addr);
+        #[cfg(not(target_arch = "wasm32"))]
+        let stream = TokioIo::new(stream);
+
+        let srv = self.connection_builder.clone();
+
+        tokio::spawn(async move {
+          if let Err(e) = srv.serve_connection(stream, service_fn(handle_request)).await {
+            eprint!("Error handling request from {}: {}", client_addr, e);
+          };
+        });
+      }
+
+      Ok(()) as anyhow::Result<()>
+    };
+    listener_service.await?;
+    Ok(())
+  }
+}
+
+fn bind_tcp_socket(listening_on: &SocketAddr) -> anyhow::Result<TcpSocket> {
+  let tcp_socket = if listening_on.is_ipv6() {
+    TcpSocket::new_v6()
+  } else {
+    TcpSocket::new_v4()
+  }?;
+  tcp_socket.set_reuseaddr(true)?;
+
+  #[cfg(not(target_arch = "wasm32"))]
+  tcp_socket.set_reuseport(true)?;
+
+  if let Err(e) = tcp_socket.bind(*listening_on) {
+    eprintln!("Failed to bind TCP socket: {}", e);
+    bail!("Failed to bind TCP socket");
+  };
+  Ok(tcp_socket)
+}
